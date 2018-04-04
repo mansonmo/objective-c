dd5b019139703871391a910874b20a279bd66c54
mansonmo <mansonmo@163.com>
2018-04-04 16:08:15 +0800

    Update main.m

--- main.m
+++ main.m
@@ -7,16 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "BNRPerson.h"
+//#import "BNRPerson.h"
+#import "BNREmployee.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        BNRPerson *mikey=[[BNRPerson alloc] init];
+        BNRPerson *mikey=[[BNREmployee alloc] init];
+        
         mikey.weightInKilos=96;
         mikey.heightInMeters=1.8;
-        float height=mikey.heightInMeters;
-        int weight=mikey.weightInKilos;
-        NSLog(@"mikey is %.2f meters tall and weighs %d kilograms",height,weight);
+        
+        NSLog(@"mikey has a weight of %d",mikey.weightInKilos);
+        NSLog(@"mikey has a height of %f",mikey.heightInKilos);
         
         float bmi = [mikey bodyMassIndex];
         NSLog(@"mikey has a BMI of %f",bmi);
